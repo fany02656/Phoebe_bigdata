b57c6424364dd4d9e70a7e191d0ce28d5fd2e412
unknown <fany02656@gmail.com>
2021-05-03 00:15:11 +0800

    dataload.py

--- dataload.py
+++ dataload.py
@@ -15,78 +15,76 @@ def sub_sample(data_w, data_i, frac=0.01):
 def main(spark):
 
 # Load all the Data
-#     train = spark.read.parquet(f'hdfs:/user/bm106/pub/MSD/cf_train.parquet')
-#     train.createOrReplaceTempView('train')
-#     print('Successfully load train data')
-#     train.show(1)
-#     val = spark.read.parquet(f'hdfs:/user/bm106/pub/MSD/cf_validation.parquet')
-#     val.createOrReplaceTempView('val')
-#     val.show(1)
-#     print('Successfully load val data')
-#     test = spark.read.parquet(f'hdfs:/user/bm106/pub/MSD/cf_test.parquet')
-#     test.createOrReplaceTempView('test')
-#     test.show(1)
-#     print('Successfully load test data')
-#
-# # count three df
-#     print('number of data in train: {}, number of data in val: {}, number of \
-#     data in test: {}'.format(train.count(), val.count(), test.count()))
-#
-# #  add another columns user_i, item_i and transform them into int
-#
-#     users = (train.select('user_id').union(val.select('user_id'))
-#             .union(test.select('user_id'))).distinct()
-#     tracks = (train.select('track_id').union(val.select('track_id'))
-#              .union(test.select('track_id'))).distinct()
-#
-#     user_encoding = StringIndexer(inputCol="user_id", outputCol="user_i")
-#     tran_user = user_encoding.fit(users)
-#     track_encoding = StringIndexer(inputCol="track_id", outputCol="track_i")
-#     tran_track = track_encoding.fit(tracks)
-#
-#     ## transform train
-#     train = tran_user.transform(train)
-#     train = tran_track.transform(train)
-#
-#     ## transform val
-#     val = tran_user.transform(val)
-#     val = tran_track.transform(val)
-#
-#     ## transform test
-#     test = tran_user.transform(test)
-#     test = tran_track.transform(test)
-#
-#     train_encode = train.withColumn("user_i", train["user_i"].cast(IntegerType())) \
-#         .withColumn("track_i", train["track_i"].cast(IntegerType()))
-#     val_encode = val.withColumn("user_i", val["user_i"].cast(IntegerType())) \
-#         .withColumn("track_i", val["track_i"].cast(IntegerType()))
-#     test_encode = test.withColumn("user_i", test["user_i"].cast(IntegerType())) \
-#         .withColumn("track_i", test["track_i"].cast(IntegerType()))
-#
-#     print('show train_encode')
-#     train_encode.show(1)
-#     print('show val_encode')
-#     val_encode.show(1)
-#     print('show test_encode')
-#     test_encode.show(1)
-#     print('Successfully convert str to int')
-#     print(train_encode.dtypes)
-#
-# # save dataset
-#     train_encode.write.parquet('train_encode.parquet')
-#     val_encode.write.parquet('val_encode.parquet')
-#     test_encode.write.parquet('test_encode.parquet')
-#     print('Successfully convert dataset and save')
+    train = spark.read.parquet(f'hdfs:/user/bm106/pub/MSD/cf_train.parquet')
+    train.createOrReplaceTempView('train')
+    print('Successfully load train data')
+    train.show(1)
+    val = spark.read.parquet(f'hdfs:/user/bm106/pub/MSD/cf_validation.parquet')
+    val.createOrReplaceTempView('val')
+    val.show(1)
+    print('Successfully load val data')
+    test = spark.read.parquet(f'hdfs:/user/bm106/pub/MSD/cf_test.parquet')
+    test.createOrReplaceTempView('test')
+    test.show(1)
+    print('Successfully load test data')
+
+# count three df
+    print('number of data in train: {}, number of data in val: {}, number of \
+    data in test: {}'.format(train.count(), val.count(), test.count()))
+
+#  add another columns user_i, item_i and transform them into int
+
+    users = (train.select('user_id').union(val.select('user_id'))
+            .union(test.select('user_id'))).distinct()
+    tracks = (train.select('track_id').union(val.select('track_id'))
+             .union(test.select('track_id'))).distinct()
+
+    user_encoding = StringIndexer(inputCol="user_id", outputCol="user_i")
+    tran_user = user_encoding.fit(users)
+    track_encoding = StringIndexer(inputCol="track_id", outputCol="track_i")
+    tran_track = track_encoding.fit(tracks)
+
+    ## transform train
+    train = tran_user.transform(train)
+    train = tran_track.transform(train)
+
+    ## transform val
+    val = tran_user.transform(val)
+    val = tran_track.transform(val)
+
+    ## transform test
+    test = tran_user.transform(test)
+    test = tran_track.transform(test)
+
+    train_encode = train.withColumn("user_i", train["user_i"].cast(IntegerType())) \
+        .withColumn("track_i", train["track_i"].cast(IntegerType()))
+    val_encode = val.withColumn("user_i", val["user_i"].cast(IntegerType())) \
+        .withColumn("track_i", val["track_i"].cast(IntegerType()))
+    test_encode = test.withColumn("user_i", test["user_i"].cast(IntegerType())) \
+        .withColumn("track_i", test["track_i"].cast(IntegerType()))
+
+    print('show train_encode')
+    train_encode.show(1)
+    print('show val_encode')
+    val_encode.show(1)
+    print('show test_encode')
+    test_encode.show(1)
+    print('Successfully convert str to int')
+    print(train_encode.dtypes)
+
+# save dataset
+    train_encode.write.parquet('train_encode.parquet')
+    val_encode.write.parquet('val_encode.parquet')
+    test_encode.write.parquet('test_encode.parquet')
+    print('Successfully convert dataset and save')
 
     
 # subsample
 
     train_path = 'train_encode.parquet'
     val_path = 'val_encode.parquet'
-    test_path='test_encode.parquet'
     train_encode = spark.read.parquet(train_path)
     val_encode = spark.read.parquet(val_path)
-    test_encode=spark.read.parquet(test_path)
     print('load success')
 
     t_v_user = val_encode.withColumnRenamed('user_i', 'user_i_').select('user_i_').distinct()
@@ -105,27 +103,6 @@ def main(spark):
     train25_encode.write.parquet('train25_encode.parquet')
     print('Successfully save subsample')
 
-    # # frac 1%
-    # train_w_1 = train_w.sample(withReplacement=False, fraction=0.01)
-    # train_1 = train_i.union(train_w_1)
-    # print('Successfully loaded 1% data')
-    # train_1.show(1)
-    # print(train_1.count())
-
-    # #frac 5%
-    # train_w_5=train_w.sample(withReplacement=False, fraction=0.05)
-    # train_5 = train_i.union(train_w_5)
-    # print('Successfully loaded 5% data')
-    # train_5.show(1)
-    # print(train_5.count())
-    #
-    #
-    # #frac 25%
-    # train_w_25=train_w.sample(withReplacement=False, fraction=0.25)
-    # train_25 = train_i.union(train_w_25)
-    # print('Successfully loaded 25% data')
-    # train_25.show(1)
-    # print(train_25.count())
 
 
 if __name__ == "__main__":
---
+++ train_eval_s5.py
@@ -0,0 +1,68 @@
+import sys
+import pyspark
+from pyspark.sql import SparkSession
+from pyspark.ml.recommendation import ALS
+from pyspark.ml.evaluation import RegressionEvaluator
+from pyspark.mllib.evaluation import RankingMetrics
+import pyspark.sql.functions as F
+from pyspark.sql.functions import expr
+import itertools
+import pandas as pd
+from pyspark.sql import SparkSession
+from pyspark.ml.recommendation import ALS, ALSModel
+from pyspark.mllib.evaluation import RankingMetrics
+from pyspark.sql import Window
+from pyspark.sql.functions import col, expr
+import pyspark.sql.functions as F
+
+
+
+def main(spark):
+    trains5_path = 'train5_encode.parquet'
+    val_path = 'val_encode.parquet'
+    test_path='test_encode.parquet'
+
+### Create dataframes
+
+    train_s5 = spark.read.parquet(trains5_path)
+    val = spark.read.parquet(val_path).select("user_i", "track_i", "count")
+    test_users = val.select("user_i").distinct()
+    order_track = Window.partitionBy('user_i').orderBy(col('count').desc())
+    # create true top 500 tracks for each users
+    true_top_track = val.select("user_i", "track_i", "count",F.rank().over(order_track).alias('rank'))\
+            .where('rank <= 500').groupBy('user_i').agg(expr('collect_list(track_i) as track_true'))
+    print('load data success')
+
+
+### Variables for hyperparameter tuning
+    rank=[i for i in range(5, 26, 10)]
+    reg_param=[10**i for i in range(-4, 0)]
+    param_grid = itertools.product(rank, reg_param)
+
+### train and evaluate models
+    for idx,i in enumerate(param_grid):
+        model_name = "model_s5_{}".format(idx)
+        ## train the model
+        als = ALS(rank=i[0], regParam=i[1], maxIter=10,
+                  userCol="user_i", itemCol="track_i", ratingCol="count",
+                  implicitPrefs=True, coldStartStrategy="drop") # The interaction data consists of implicit feedback
+        model = als.fit(train_s5)
+        print(f"the {model_name} trained with rank {i[0]}, reg_param {i[1]}.")
+        ## evaluate the model
+        # predict top 500
+        prediction = model.recommendForUserSubset(test_users, 500)
+        # join prediction and true for each user
+        pre_true_join = true_top_track.join(prediction, 'user_i') \
+            .rdd.map(lambda row: ([cobine.track_i for cobine in row["recommendations"]], row["track_true"]))
+        ## Ranking metrics
+        rankingMetrics = RankingMetrics(pre_true_join)
+        precision_at = rankingMetrics.precisionAt(500)
+        map = rankingMetrics.meanAveragePrecision
+        ndcg = rankingMetrics.ndcgAt(500)
+        print("{}:[precision_at:{}, map:{}, ndcg:{}]".format(model_name, precision_at, map, ndcg))
+
+    
+
+if __name__ == "__main__":
+    spark = SparkSession.builder.appName("phoebe").config("spark.executor.memory", '30g').config("spark.driver.memory", '30g') .getOrCreate()
+    main(spark)
